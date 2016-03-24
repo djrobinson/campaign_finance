51559155587c4e04c516a2b2ae0679c871d4dbbb
Danny Robinson <daniel@djrobinson.me>
2016-03-24 17:58:44 -0600

    setup migrations for contribs

---
+++ new/src/server/migrations/20160324174104_candidateMaster.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('candidate_master', function(table){
+    table.string('CMTE_ID');
+    table.string('CMTE_NM');
+    table.string('TRES_NM');
+    table.string('CMTE_ST1');
+    table.string('CMTE_ST2');
+    table.string('CMTE_CITY');
+    table.string('CMTE_ST');
+    table.string('CMTE_ZIP');
+    table.string('CMTE_DSGN');
+    table.string('CMTE_TP');
+    table.string('CMTE_PTY_AFFILIATION');
+    table.string('CMTE_FILING_FREQ');
+    table.string('ORG_TP');
+    table.string('CONNECTED_ORG_NM');
+    table.string('CAND_ID');
+  });
+};
+
+exports.down = function(knex, Promise) {
+
+};
