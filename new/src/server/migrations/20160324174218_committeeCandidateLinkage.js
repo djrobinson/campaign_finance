e0dcffc954179e034679e1ba785f5f6f113814dc
Danny Robinson <daniel@djrobinson.me>
2016-03-25 15:44:13 -0600

    cmte cand link migration

--- new/src/server/migrations/20160324174218_committeeCandidateLinkage.js
+++ new/src/server/migrations/20160324174218_committeeCandidateLinkage.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('cmte_cand_linkage', function(table){
+    table.string('CAND_ID');
+    table.string('CAND_ELECTION_YR');
+    table.string('FEC_ELECTION_YR');
+    table.string('CMTE_ID');
+    table.string('CMTE_TP');
+    table.string('CMTE_DSGN');
+    table.string('LINKAGE_ID');
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('cmte_cand_linkage');
 };
