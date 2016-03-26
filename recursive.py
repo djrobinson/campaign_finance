258a5b9034aa7881a741b872e7695eacee184c51
Danny Robinson <daniel@djrobinson.me>
2016-03-26 15:31:53 -0600

    Created congressional python seeder

--- recursive.py
+++ recursive.py
@@ -1,10 +1,24 @@
 # Import the os module, for the os.walk function
 import os
+from pymongo import MongoClient
+import json
+
+client = MongoClient()
+db = client.testPolis
 
 # Set the directory you want to start from
-rootDir = './votes'
+# rootDir = './votes'
+# for dirName, subdirList, fileList in os.walk(rootDir):
+#     print('Found directory: %s' % dirName)
+#     for fname in fileList:
+#       with open(dirName + "/" + fname, 'r') as fin:
+#         print fin.read()
+
+rootDir = './votes/2014/h1'
 for dirName, subdirList, fileList in os.walk(rootDir):
     print('Found directory: %s' % dirName)
     for fname in fileList:
-      with open(dirName + "/" + fname, 'r') as fin:
-        print fin.read()
+      with open('./votes/2014/h1/'+fname, 'r') as fin:
+        item = fin.read()
+        jsonitem = json.loads(item)
+        result = db.restaurants.insert_one(jsonitem)
