eeefca2308c47b707d51141b75844c368cc3f937
Danny Robinson <daniel@djrobinson.me>
2016-03-28 13:47:10 -0600

    Added linkage route/query

--- new/src/server/routes/committee_master_routes.js
+++ new/src/server/routes/committee_master_routes.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var router = express.Router();
 var query = require('../queries/committee_master_queries');
+var linkquer = require('../queries/cmte_cand_link_queries');
 
 router.get('/', function(req, res, next) {
   query.getTenCmteMster().then(function(comm){
     res.json(comm);
+  });
+});
+
+router.get('/link', function(req, res, next){
+  linkquer.getTenCmteCandLink().then(function(links){
+    res.json(links);
+  });
+});
+
+router.get('/link/:cand_id', function(req, res, next){
+  linkquer.getCmteCandLink(req.params.cand_id).then(function(links){
+    res.json(links);
   });
 });
 
