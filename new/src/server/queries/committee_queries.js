35736eaf667a93b1b498e875d141ddf525e06b0c
Danny Robinson <daniel@djrobinson.me>
2016-04-08 12:28:32 -0600

    Completed committee queries

--- new/src/server/queries/committee_queries.js
+++ new/src/server/queries/committee_queries.js
@@ -12,5 +12,12 @@ module.exports = {
     return knex('committee_registrations')
            .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
            .limit(limit);
+  },
+  getCmteSort: function(cmte_id, col){
+    return knex('committee_registrations')
+           .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
+           .limit(limit);
+           .where({'com_id': cmte_id})
+           .orderBy(column, 'desc');
   }
 };
