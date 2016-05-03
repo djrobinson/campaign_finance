bb8b6ee812584cb33130801cb3876133a4fd9f74
Danny Robinson <daniel@djrobinson.me>
2016-05-02 21:25:49 -0600

    Completed committee transfers pagination

--- new/src/server/api_funds/cmte_to_cmte.js
+++ new/src/server/api_funds/cmte_to_cmte.js
@@ -7,21 +7,42 @@ var query = require('../queries/cmte_to_cmte_queries.js');
 */
 
 router.get('/', function(req, res, next){
-  query.getCmteCmteSort().then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.getCmteCmteSort(offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.getCmteCmteSort().then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/:cmte_id/contributor', function(req, res, next){
-  query.getCmteCmteByDon(req.params.cmte_id).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.getCmteCmteByDon(req.params.cmte_id, offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.getCmteCmteByDon(req.params.cmte_id).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/:cmte_id/recipient', function(req, res, next){
-  query.getCmteCmteByRec(req.params.cmte_id).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.getCmteCmteByRec(req.params.cmte_id, offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.getCmteCmteByRec(req.params.cmte_id).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 module.exports = router;
--- new/src/server/queries/cmte_to_cmte_queries.js
+++ new/src/server/queries/cmte_to_cmte_queries.js
@@ -1,21 +1,24 @@
 var knex = require('./knex');
 
 module.exports = {
-  getCmteCmteByDon: function(cmte_id){
+  getCmteCmteByDon: function(cmte_id, offset){
     return knex('cmte_to_cmte')
            .where({'CMTE_ID': cmte_id})
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getCmteCmteByRec: function(cmte_id){
+  getCmteCmteByRec: function(cmte_id, offset){
     return knex('cmte_to_cmte')
            .where({'OTHER_ID': cmte_id})
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getCmteCmteSort: function(){
+  getCmteCmteSort: function(offset){
     return knex('cmte_to_cmte')
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   }
 }
