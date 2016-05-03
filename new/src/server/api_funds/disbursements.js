ffa7fca4836d9169786c6a458f4a87ceed2bc4c2
Danny Robinson <daniel@djrobinson.me>
2016-05-02 22:39:52 -0600

    Completed disbursements offsets

--- new/src/server/api_funds/disbursements.js
+++ new/src/server/api_funds/disbursements.js
@@ -8,50 +8,106 @@ var query = require('../queries/disbursement_queries.js')
 
 router.get('/', function(req, res, next){
   if (req.query.recipient) {
-    query.getByRec(req.query.recipient).then(function(data){
-      res.json(data);
-    });
-  } else {
-    query.getDistOrder().then(function(data){
+    if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getByRec(req.query.recipient, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+        query.getByRec(req.query.recipient).then(function(data){
+          res.json(data);
+        });
+      }
+    } else {
+      if (req.query.offset){
+        var offset = +req.query.offset * 100;
+        query.getDistOrder(offset).then(function(data){
+          res.json(data);
+        });
+      } else {
+      query.getDistOrder().then(function(data){
+        res.json(data);
+      });
+    }
+  }
+});
+
+router.get('/:cand_id/candidate', function(req, res, next){
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getDisbByCand(req.params.cand_id, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.getDisbByCand(req.params.cand_id).then(function(data){
       res.json(data);
     });
   }
 });
 
-router.get('/:cand_id/candidate', function(req, res, next){
-  query.getDisbByCand(req.params.cand_id).then(function(data){
-    res.json(data);
-  });
-});
-
 router.get('/type/:cat_cod', function(req, res, next){
-  query.getByCode(req.params.cat_cod).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getByCode(req.params.cat_cod, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.getByCode(req.params.cat_cod).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/:cand_id/candidate/type', function(req, res, next){
-  query.aggByType(req.params.cand_id).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.aggByType(req.params.cand_id, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.aggByType(req.params.cand_id).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/:cand_id/candidate/purpose', function(req, res, next){
-  query.aggByPurp(req.params.cand_id).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.aggByPurp(req.params.cand_id, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.aggByPurp(req.params.cand_id).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/aggregate/type', function(req, res, next){
-  query.typeTotals().then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.typeTotals(offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.typeTotals().then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/aggregate/purpose', function(req, res, next){
-  query.purpTotals().then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.purpTotals(offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+    query.purpTotals().then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 module.exports = router;
--- new/src/server/queries/disbursement_queries.js
+++ new/src/server/queries/disbursement_queries.js
@@ -1,57 +1,69 @@
 var knex = require('./knex');
 
 module.exports = {
-  getDistOrder: function(){
+  getDistOrder: function(offset){
     return knex('candidate_disbursements')
           .orderBy('dis_amo', 'desc')
-          .limit(100);
+          .limit(100)
+          .offset(offset);
   },
-  getDisbByCand: function(cand_id){
+  getDisbByCand: function(cand_id, offset){
     return knex('candidate_disbursements')
           .where({'can_id': cand_id})
           .orderBy('dis_amo', 'desc')
-          .limit(100);
+          .limit(100)
+          .offset(offset);
   },
-  getByRec: function(rec_nam){
+  getByRec: function(rec_nam, offset){
     return knex('candidate_disbursements')
            .where({'rec_nam': rec_nam})
            .orderBy('dis_amo', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getByCode: function(cat_cod){
+  getByCode: function(cat_cod, offset){
     return knex('candidate_disbursements')
            .where({'cat_cod': cat_cod})
            .orderBy('dis_amo', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  aggByType: function(cand_id){
+  aggByType: function(cand_id, offset){
     return knex('candidate_disbursements')
            .select('cat_des')
            .where({'can_id': cand_id})
            .sum('dis_amo')
            .groupBy('cat_des')
            .orderBy('sum', 'desc')
+           .limit(100)
+           .offset(offset);
   },
-  aggByPurp: function(cand_id){
+  aggByPurp: function(cand_id, offset){
     return knex('candidate_disbursements')
            .select('dis_pur_des')
            .where({'can_id': cand_id})
            .sum('dis_amo')
            .groupBy('dis_pur_des')
            .orderBy('sum', 'desc')
+           .limit(100)
+           .offset(offset);
   },
-  typeTotals: function(){
+  typeTotals: function(offset){
     return knex('candidate_disbursements')
            .select('cat_des')
            .sum('dis_amo')
            .groupBy('cat_des')
            .orderBy('sum', 'desc')
+           .limit(100)
+           .offset(offset);
   },
-  purpTotals: function(){
+  purpTotals: function(offset){
         return knex('candidate_disbursements')
            .select('dis_pur_des')
            .sum('dis_amo')
            .groupBy('dis_pur_des')
            .orderBy('sum', 'desc')
+           .limit(100)
+           .offset(offset);
   }
 };
