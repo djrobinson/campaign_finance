772d1bcdf1e0ece5463d6daec12fa2f4f4824c59
Danny Robinson <daniel@djrobinson.me>
2016-05-02 22:06:42 -0600

    Added committees offsets

--- new/src/server/api_funds/committees.js
+++ new/src/server/api_funds/committees.js
@@ -10,31 +10,66 @@ OFFSET ON MAIN, SORT
 
 router.get('/', function(req, res, next){
   if (req.query.limit){
-    query.getCmteLimit(req.query.limit).then(function(summs){
-      res.json(summs);
-    });
+    if (req.query.offset){
+      var offset = +req.query.offset * req.query.limit;
+      query.getCmteLimit(req.query.limit, offset).then(function(summs){
+          res.json(summs);
+        });
+    } else {
+      query.getCmteLimit(req.query.limit).then(function(summs){
+        res.json(summs);
+      });
+    }
   } else {
-    query.getCmteLimit().then(function(summs){
-      res.json(summs);
-    });
+    if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getCmteLimit(null, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+      query.getCmteLimit().then(function(summs){
+        res.json(summs);
+      });
+    }
   }
 });
 
 router.get('/:cmte_id', function(req, res, next){
-  query.getCmteSum(req.params.cmte_id).then(function(summ){
-    res.json(summ);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.getCmteSum(req.params.cmte_id, offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.getCmteSum(req.params.cmte_id).then(function(summ){
+      res.json(summ);
+    });
+  }
 });
 
 router.get('/sort/:col_name', function(req, res, next){
   if (req.query.limit){
-    query.getCmteSort(req.query.limit, req.params.col_name).then(function(summs){
-      res.json(summs);
-    });
+    if (req.query.offset){
+      var offset = +req.query.offset * req.query.limit;
+      query.getCmteSort(req.query.limit, req.params.col_name, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+      query.getCmteSort(req.query.limit, req.params.col_name).then(function(summs){
+        res.json(summs);
+      });
+    }
   } else {
-    query.getCmteSort(null, req.params.col_name).then(function(summs){
-      res.json(summs);
-    });
+    if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getCmteSort(null, req.params.col_name, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+      query.getCmteSort(null, req.params.col_name).then(function(summs){
+        res.json(summs);
+      });
+    }
   }
 });
 
--- new/src/server/queries/committee_queries.js
+++ new/src/server/queries/committee_queries.js
@@ -2,23 +2,26 @@ var knex = require('./knex');
 
 
 module.exports = {
-   getCmteSum: function(cmte_id){
+   getCmteSum: function(cmte_id, offset){
     return knex('committee_registrations')
            .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
            .where({'com_id': cmte_id})
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getCmteLimit: function(limit){
-    if (!limit) { limit = 100; }
-    return knex('committee_registrations')
-           .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
-           .limit(limit);
-  },
-  getCmteSort: function(limit, col){
+  getCmteLimit: function(limit, offset){
     if (!limit) { limit = 100; }
     return knex('committee_registrations')
            .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
            .limit(limit)
-           .orderBy(col, 'desc');
+           .offset(offset);
+  },
+  getCmteSort: function(limit, col, offset){
+    if (!limit) { limit = 100; }
+    return knex('committee_registrations')
+           .innerJoin('committee_summaries', 'COMMITTEE_ID', 'com_id')
+           .limit(limit)
+           .orderBy(col, 'desc')
+           .offset(offset);
   }
 };
