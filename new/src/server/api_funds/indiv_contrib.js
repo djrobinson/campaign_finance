4550a7c653d85aa0ef469e9b759853397126f13f
Danny Robinson <daniel@djrobinson.me>
2016-05-02 21:40:50 -0600

    Individual contributions

--- new/src/server/api_funds/indiv_contrib.js
+++ new/src/server/api_funds/indiv_contrib.js
@@ -8,41 +8,83 @@ var query = require('../queries/indiv_contrib_queries.js');
 
 router.get('/', function(req, res, next){
   if (req.query.donor){
-    query.getContribByDon(req.query.donor.toUpperCase()).then(function(data){
+    if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getCmteCmteSort(null, offset).then(function(data){
+        res.json(data);
+      });
+    } else {
+      query.getContribByDon(req.query.donor.toUpperCase(), offset).then(function(data){
+        res.json(data);
+      });
+    }
+  } else {
+    if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getIndivContribs(offset).then(function(data){
       res.json(data);
     });
-  } else {
-    query.getIndivContribs().then(function(data){
+    } else {
+      query.getIndivContribs().then(function(data){
+        res.json(data);
+      });
+    }
+  }
+});
+
+router.get('/:cmte_id/recipient', function(req, res, next){
+  if (req.query.offset){
+      var offset = +req.query.offset * 100;
+      query.getIndivContrib(req.params.cmte_id, offset).then(function(data){
+          res.json(data);
+        });
+    } else {
+    query.getIndivContrib(req.params.cmte_id).then(function(data){
       res.json(data);
     });
   }
 });
 
-router.get('/:cmte_id/recipient', function(req, res, next){
-  query.getIndivContrib(req.params.cmte_id).then(function(data){
-    res.json(data);
-  });
-});
-
 //TODO SEARCH BY EMPLOYER, OCCUPATION
 
 router.get('/employer', function(req, res, next){
-  query.aggregateEmpl().then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.aggregateEmpl(offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.aggregateEmpl().then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 router.get('/occupation', function(req, res, next){
-  query.aggregateTitle().then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.aggregateTitle(offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.aggregateTitle().then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 
 router.get('/committee/:cmte_id', function(req, res, next){
-  query.indivByCmte(req.params.cmte_id).then(function(data){
-    res.json(data);
-  });
+  if (req.query.offset){
+    var offset = +req.query.offset * 100;
+    query.indivByCmte(req.params.cmte_id, offset).then(function(data){
+      res.json(data);
+    });
+  } else {
+    query.indivByCmte(req.params.cmte_id).then(function(data){
+      res.json(data);
+    });
+  }
 });
 
 
--- new/src/server/queries/indiv_contrib_queries.js
+++ new/src/server/queries/indiv_contrib_queries.js
@@ -1,47 +1,54 @@
 var knex = require('./knex');
 
 module.exports = {
-  getIndivContrib: function(cmte_id){
+  getIndivContrib: function(cmte_id, offset){
     return knex('indiv_contrib')
            .where({'CMTE_ID': cmte_id})
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getIndivContribs: function(){
+  getIndivContribs: function(offset){
     return knex('indiv_contrib')
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  getContribByDon: function(donor){
+  getContribByDon: function(donor, offset){
     return knex('indiv_contrib')
-           .where('NAME', 'like', '%'+donor+'%');
+           .where('NAME', 'like', '%'+donor+'%')
+           .offset(offset);
   },
-  aggregateEmpl: function(donor){
+  aggregateEmpl: function(donor, offset){
     return knex('indiv_contrib')
            .select('EMPLOYER')
            .sum('TRANSACTION_AMT')
            .groupBy('EMPLOYER')
            .orderBy('sum', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  aggregateTitle: function(donor){
+  aggregateTitle: function(offset){
     return knex('indiv_contrib')
            .select('OCCUPATION')
            .sum('TRANSACTION_AMT')
            .groupBy('OCCUPATION')
            .orderBy('sum', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  singleEmpl: function(employ){
+  singleEmpl: function(employ, offset){
     return knex('indiv_contrib')
            .where({'EMPLOYER': employ})
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   },
-  indivByCmte: function(cmte_id){
+  indivByCmte: function(cmte_id, offset){
     return knex('indiv_contrib')
            .where({'CMTE_ID': cmte_id})
            .orderBy('TRANSACTION_AMT', 'desc')
-           .limit(100);
+           .limit(100)
+           .offset(offset);
   }
 }
