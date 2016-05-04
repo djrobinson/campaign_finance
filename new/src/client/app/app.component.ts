6628187e44a71302718604e381f10df55cc42794
Danny Robinson <daniel@djrobinson.me>
2016-05-03 21:26:22 -0600

    Styled main boxes

--- new/src/client/app/app.component.ts
+++ new/src/client/app/app.component.ts
@@ -7,16 +7,31 @@ import {SubjectService} from './subject.service';
   styles: [`
     .app {
       display: block;
-      text-align: center;
+      position: absolute;
       padding: 25px;
       background: #f5f5f5;
+      text-align: center;
+      height: 100%;
     }
+    .tile {
+      display: inline-flex;
+      justify-content: space-around;
+      flex-direction: row;
+      text-align: center;
+      flex-wrap: wrap;
+      width: 15%;
+      height: 8rem;
+
+      margin: .5rem;
+      background: #7A8B99;
+      color: #A9DDD6;
+      font-family: "Courier New", Courier, monospace;
+    }
+
   `],
   template: `
     <div class="app">
-      <ul class="subjects">
-        <li *ngFor="#subject of subjects">{{subject.name}}</li>
-      </ul>
+      <div class="tile" *ngFor="#subject of subjects"><h2>{{subject.name}}</h2></div>
     </div>
   `,
   providers: [SubjectService]
