12f349fee7815b09ad7d6bf7feb3827010e102c0
Danny Robinson <daniel@djrobinson.me>
2016-05-22 13:18:53 -0600

    Got committee tables appearing on popup

--- src/client/app/graphs/committee-popup.component.ts
+++ src/client/app/graphs/committee-popup.component.ts
@@ -7,7 +7,21 @@ import {TitleService} from '../api_services/title.service';
   selector: 'committee-popup',
   template: `
     <div class="row">
-      <h1>Committee Popup</h1>
+      <div class="four columns">
+        <ul *ngFor="#individual of individuals?.data">
+          <li>{{individual.NAME}} {{individual.TRANSACTION_AMT}}</li>
+        </ul>
+      </div>
+      <div class="four columns">
+        <h4>Committee Financials</h4>
+        <h5>{{committee?.COMMITEE_EMAIL}}</h5>
+        <h5>{{committee?.net_con}}</h5>
+      </div>
+      <div class="four columns">
+        <ul *ngFor="#exp of opex?.data">
+          <li>{{exp.NAME}}</li>
+        </ul>
+      </div>
     </div>
     <div class="row indiv twelve columns">
       <button (click)="close()">Close</button>
@@ -24,6 +38,8 @@ export class CommitteePopupComponent implements OnInit, OnChanges {
   @Input() committee: string;
   @Output() exitEmit = new EventEmitter();
   private committee: Observable<Object>;
+  private opex = {};
+  private individuals = {};
 
 
   constructor(private _TitleService: TitleService,
@@ -35,10 +51,16 @@ export class CommitteePopupComponent implements OnInit, OnChanges {
       this.http.get('/api/committees/'+this.committee).map((res: Response) => res.json()),
       this.http.get('/api/individuals/committee/'+this.committee).map((res: Response) => res.json()),
       this.http.get('/api/transfers/'+this.committee+'/contributor').map((res: Response) => res.json()),
-      this.http.get('/api/transfers/'+this.committee+'/recipient').map((res: Response) => res.json())
+      this.http.get('/api/transfers/'+this.committee+'/recipient').map((res: Response) => res.json()),
+      this.http.get('/api/opex/committee/'+this.committee).map((res: Response) => res.json())
+
     ).subscribe(
       data => {
         console.log(data);
+        this.committee
+        this.committee = data[0][0];
+        this.opex.data = data[4];
+        this.individuals.data = data[1];
       },
       err => console.error(err)
       );
