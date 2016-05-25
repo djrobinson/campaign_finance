6a638231755e9883132a62e0beef8c4af0582be8
Danny Robinson <daniel@djrobinson.me>
2016-05-24 21:34:14 -0600

    lobbying, bill tables

--- src/server/migrations/20160524211918_lobby_bill.js
+++ src/server/migrations/20160524211918_lobby_bill.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('lobby_bill', function(table){
+    table.string("bill_id");
+    table.string("issue_id");
+    table.string("congress_no");
+    table.string("bill_type_raw");
+    table.string("bill_type");
+    table.string("bill_no");
+    table.string("bill_name");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTable('lobby_bill');
 };
--- src/server/migrations/20160524211925_lobbying.js
+++ src/server/migrations/20160524211925_lobbying.js
@@ -1,8 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('lobbying', function(table){
+    table.string("transaction_id");
+    table.string("transaction_type");
+    table.string("transaction_type_desc");
+    table.string("year");
+    table.string("filing_type");
+    table.string("filing_included_nsfs");
+    table.string("amount");
+    table.string("registrant_name");
+    table.string("registrant_is_firm");
+    table.string("client_name");
+    table.string("client_category");
+    table.string("client_parent_name");
+    table.string("include_in_industry_totals");
+    table.string("use");
+    table.string("affiliate");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTable('lobbying');
 };
