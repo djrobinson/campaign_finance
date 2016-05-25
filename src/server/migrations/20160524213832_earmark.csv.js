1a6dde26a214b77b1a9c8b59d45b68da3064daea
Danny Robinson <daniel@djrobinson.me>
2016-05-24 21:49:10 -0600

    earmark and location tables

--- src/server/migrations/20160524213832_earmark.csv.js
+++ src/server/migrations/20160524213832_earmark.csv.js
@@ -1,8 +1,32 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('earmark', function(table){
+    table.string("id");
+    table.string("import_reference_id");
+    table.number("fiscal_year");
+    table.string("budget_amount");
+    table.string("house_amount");
+    table.string("senate_amount");
+    table.number("omni_amount");
+    table.number("final_amount");
+    table.string("bill");
+    table.string("bill_section");
+    table.string("bill_subsection");
+    table.string("description");
+    table.string("notes");
+    table.string("presidential");
+    table.string("undisclosed");
+    table.string("house_members");
+    table.string("house_parties");
+    table.string("house_states");
+    table.string("house_districts");
+    table.string("senate_members");
+    table.string("house_states");
+    table.string("senate_parties");
+    table.string("senate_states");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTable('earmark');
 };
