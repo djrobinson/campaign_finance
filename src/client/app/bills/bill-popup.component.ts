936158300356117a16a23a61f0ba54e0a8d32d08
Danny Robinson <daniel@djrobinson.me>
2016-05-25 15:29:24 -0600

    Added data to page for bill popup view

--- src/client/app/bills/bill-popup.component.ts
+++ src/client/app/bills/bill-popup.component.ts
@@ -10,6 +10,39 @@ import {Http, Response} from 'angular2/http';
   template: `
             <h1>Bills Popup</h1>
             <div class="row">
+              <div class="four columns">
+                <h2>Bill Overview</h2>
+                <h3>{{billData?.data?.official_title}}</h3>
+                <h5>{{billData?.data?.subjects_top_term}}</h5>
+                <h5>{{billData?.data?.bill_id}}</h5>
+                <ul>
+                  <li *ngFor="#sponsor of billData?.data?.cosponsors">
+                    <p>{{sponsor.name}}</p>
+                  </li>
+                </ul>
+                <ul>
+                  <li *ngFor="#action of billData?.data?.actions">
+                    <p>{{action.text}}</p>
+                  </li>
+                </ul>
+              </div>
+              <div class="four columns">
+                <h2>Bill Votes</h2>
+                <h5>{{billVotes?.data?.category}}</h5>
+                <h5>{{billVotes?.data?.question}}</h5>
+                <h5>{{billVotes?.data?.result_text}}</h5>
+                <h5>{{billVotes?.data?.source_url}}</h5>
+                <h5>{{billVotes?.data?.requires}}</h5>
+              </div>
+              <div class="four columns">
+                <h2>Lobbyist Issues w/ Bill</h2>
+                <ul>
+                  <li *ngFor="#issue of lobbyIssues?.data">
+                    <p>{{issue.bill_id}} {{issue.issue_id}}</p>
+                  </li>
+                </ul>
+              </div>
+
             </div>
            `,
   providers: [TitleService],
@@ -17,6 +50,10 @@ import {Http, Response} from 'angular2/http';
 })
 export class BillPopupComponent implements OnInit {
   bill_id: string;
+  billData: Observable<Object> = {};
+  billVotes: Observable<Object> = {};
+  lobbyIssues: Observable<Object> = {};
+
   constructor(private _titleService: TitleService
               private params: RouteParams
               private http: Http ) {
@@ -39,7 +76,11 @@ export class BillPopupComponent implements OnInit {
 
     ).subscribe(
       data => {
+        this.billData.data = data[1][0];
+        this.billVotes.data = data[0][0];
+        this.lobbyIssues.data = data[2];
         console.log(data);
+
       },
       err => console.error(err)
     );
