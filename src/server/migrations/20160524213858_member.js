0b566905da22edcb9b9ec764633a0b218ba4d489
Danny Robinson <daniel@djrobinson.me>
2016-05-24 21:53:14 -0600

    recipient and member tables

--- src/server/migrations/20160524213858_member.js
+++ src/server/migrations/20160524213858_member.js
@@ -1,8 +1,18 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('member', function(table){
+    table.string("id");
+    table.string("earmark_id");
+    table.number("raw_name");
+    table.string("crp_id");
+    table.string("crstandardized_namep_id");
+    table.string("chamber");
+    table.string("party");
+    table.string("state");
+    table.string("district");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTable('member');
 };
--- src/server/migrations/20160524213909_recipient.js
+++ src/server/migrations/20160524213909_recipient.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipient', function(table){
+    table.string("id");
+    table.string("earmark_id");
+    table.number("raw_recipient");
+    table.string("standardized_recipient");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.dropTable('recipient');
 };
