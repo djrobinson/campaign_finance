1602fc1ee317e29e835d4aa14469941acab3dfc8
Danny Robinson <daniel@djrobinson.me>
2016-05-25 22:50:01 -0600

    Added treemap and piechart components

---
+++ src/client/app/graphs/charts/pie-chart.component.ts
@@ -0,0 +1,29 @@
+import {Component, Input, OnInit, EventEmitter} from 'angular2/core';
+
+@Component({
+  selector: 'pie-chart',
+  template: `
+    <div class="row">
+
+    </div>
+    <div class="row indiv twelve columns">
+      <button (click)="close()">Close</button>
+    </div>
+  `,
+  styles: [`
+    .indiv {
+      text-align: center;
+    }
+  `]
+})
+export class PieComponent implements OnInit, OnChanges {
+  //May want to start creating individual/committee types.
+
+  close() {
+    console.log("CLOSE");
+    this.exitEmit.emit({
+      exit: true
+    });
+  }
+
+}
