8b31ae2dbfb9d305e0d42d43aa4b0fe63278b4e9
Danny Robinson <daniel@djrobinson.me>
2016-05-26 22:26:30 -0600

    Changed layout

--- src/client/app/graphs/candidate-popup.component.ts
+++ src/client/app/graphs/candidate-popup.component.ts
@@ -9,58 +9,52 @@ import {PieComponent} from './charts/pie-chart.component';
 @Component({
   selector: 'candidate-popup',
   template: `
-     <div class="cand-style">
+    <div class="cand-style">
+    <div class="nine columns">
       <div class="row">
         <div class="three columns">
             <h4>{{candidate?.CANDIDATE_NAME}}</h4>
             <img [src]="imageVar?.image" alt="picture">
         </div>
-        <div class="three columns">
-              <p>Office Sought: {{candidate?.CANDIDATE_OFFICE}}</p>
-              <p>Hometown: {{candidate?.can_cit}}, {{candidate?.STATE}}</p>
-              <p>Birthdate: {{candidateInfo?.bio?.birthday}}</p>
-              <p>Gender: {{candidateInfo?.bio?.gender}}</p>
-              <p>Religion: {{candidateInfo?.bio?.religion}}</p>
-              <p><a [href]="candidate?.lin_ima">FEC Filing Link</a></p>
-              <p><em>Begin Cash: </em> {{parseFloat(candidate?.cas_on_han_clo_of_per) | currency:'USD':true}}</p>
-              <p><em>End Cash: </em> {{parseFloat(candidate?.cas_on_han_clo_of_per) | currency:'USD':true}}</p>
-              <p><em>Debt: </em> {{parseFloat(candidate?.deb_owe_by_com) | currency:'USD':true}}</p>
-              <p><em>Funding from Candidate: </em> {{parseFloat(candidate?.can_con) | currency:'USD':true}}</p>
-              <p><em>Net Contributions: </em> {{parseFloat(candidate?.net_con) | currency:'USD':true}}</p>
-              <p><em>Net Opex: </em> {{parseFloat(candidate?.net_ope_exp) | currency:'USD':true}}</p>
-              <p><em>Distributions: </em> {{parseFloat(candidate?.tot_dis) | currency:'USD':true}}</p>
+        <div class="three columns pie">
+          <pie-chart>
+          </pie-chart>
         </div>
         <div class="three columns">
+          <p>Office Sought: {{candidate?.CANDIDATE_OFFICE}}</p>
+          <p>Hometown: {{candidate?.can_cit}}, {{candidate?.STATE}}</p>
+          <p>Birthdate: {{candidateInfo?.bio?.birthday}}</p>
+          <p>Gender: {{candidateInfo?.bio?.gender}}</p>
+          <p>Religion: {{candidateInfo?.bio?.religion}}</p>
+          <p><a [href]="candidate?.lin_ima">FEC Filing Link</a></p>
+          <p><em>Begin Cash: </em> {{parseFloat(candidate?.cas_on_han_clo_of_per) | currency:'USD':true}}</p>
+          <p><em>End Cash: </em> {{parseFloat(candidate?.cas_on_han_clo_of_per) | currency:'USD':true}}</p>
+          <p><em>Debt: </em> {{parseFloat(candidate?.deb_owe_by_com) | currency:'USD':true}}</p>
+          <p><em>Funding from Candidate: </em> {{parseFloat(candidate?.can_con) | currency:'USD':true}}</p>
+          <p><em>Net Contributions: </em> {{parseFloat(candidate?.net_con) | currency:'USD':true}}</p>
+          <p><em>Net Opex: </em> {{parseFloat(candidate?.net_ope_exp) | currency:'USD':true}}</p>
+          <p><em>Distributions: </em> {{parseFloat(candidate?.tot_dis) | currency:'USD':true}}</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="six columns treemap">
+          <treemap route="/api/pac/aggregate/P00003392">
+          </treemap>
+        </div>
+      </div>
+    </div>
+      <div class="three columns">
+        <h3>Usage of Funds</h3>
+        <div class="table-div">
+          <ul>
+            <li *ngFor="#pacspend of pacSpends">{{pacspend.pay}} {{pacspend.exp_amo}}</li>
+          </ul>
+        </div>
+      </div>
 
-          <h3>Usage of Funds</h3>
-          <div class="table-div">
-            <ul>
-              <li *ngFor="#pacspend of pacSpends">{{pacspend.pay}} {{pacspend.exp_amo}}</li>
-            </ul>
-          </div>
-        </div>
-        <div class="three columns table-div">
-          <h3>Holder</h3>
-        </div>
-       </div>
-        <div class="row">
-           <div class="three columns pie">
-              <pie-chart>
-              </pie-chart>
-           </div>
-           <div class="three columns">
-            <h1>Here's the holder</h1>
-           </div>
-           <div class="six columns">
-            <div class="table-div">
-              <treemap route="/api/pac/aggregate/P00003392">
-              </treemap>
-            </div>
-           </div>
-        </div>
-        <div class="row indiv twelve columns">
-          <button (click)="close()">Close</button>
-        </div>
+      <div class="row indiv twelve columns">
+        <button (click)="close()">Close</button>
+      </div>
     </div>
   `,
   directives: [TreemapComponent, PieComponent],
@@ -83,10 +77,13 @@ import {PieComponent} from './charts/pie-chart.component';
       font-size: 8px;
     }
     .table-div {
-      height: 300px;
+      height: 600px;
       overflow: scroll;
     }
     .pie {
+      height: 300px;
+    }
+    .treemap {
       height: 300px;
     }
   `]
