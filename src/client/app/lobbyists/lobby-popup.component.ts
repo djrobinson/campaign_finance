dc108039fbb5231ce4a8612acfa986ed28a9f413
Danny Robinson <daniel@djrobinson.me>
2016-06-01 05:17:32 -0600

    Completed lobbyist popup positioning

--- src/client/app/lobbyists/lobby-popup.component.ts
+++ src/client/app/lobbyists/lobby-popup.component.ts
@@ -4,44 +4,35 @@ import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'lobby-popup',
-  template: `
-          <div class="lobby-popup-container">
-            <div class="row">
-              <div class="four columns">
-                 <h2>Sponsors</h2>
-              </div>
-              <div class="four columns">
-                <h4>Lobby Issue Info</h4>
-                <h5>{{billId}}</h5>
-                <h5>{{currLobby?.data?.client_parent_name}}</h5>
-              </div>
-              <div class="other-lobbies-container">
-              <h4>Other Bills Lobbied for Client</h4>
-                <div *ngFor="#trans of transactionBills?.data" class="other-lobbies">
-                  <h5>{{trans.bill_name}}</h5>
-                  <p>{{trans.specific_issue}}</p>
-                  <p>{{trans.category}}</p>
-                </div>
-              </div>
-            </div>
-            <div class="row indiv twelve columns">
-              <button (click)="close()">Close</button>
-            </div>
-          </div>
-  `,
+  templateUrl: 'app/lobbyists/lobby-popup.html',
   styles: [`
+    .lobbyist {
+      position: absolute;
+      width: 50%;
+      left: 0;
+    }
     .other-lobbies {
       width: 100%;
-      height: 200px;
       border: solid 1px gray;
       overflow: hidden;
     }
     .other-lobbies-container {
       position: absolute;
       height: 100%;
-      width: 33.33%;
+      width: 50%;
       right: 0;
       overflow: scroll;
+    }
+    .lobby-popup-container {
+      position: relative;
+      height: 90%;
+      width: 100%;
+    }
+    .indiv {
+      position: absolute;
+      text-align: center;
+      height: 5%;
+      bottom: 0;
     }
   `],
   directives: []
@@ -52,28 +43,24 @@ export class LobbyPopupComponent implements OnInit, OnChanges {
   @Input() billId: string;
   @Output() exitEmit = new EventEmitter();
   private transactionBills: Object = {};
-  private currLobby: Object = {};
+  private first: Object = {};
 
   constructor(private http: Http) {
-    // this.parseFloat = function(num) {
-    //   return parseFloat(num);
-    // }
+    this.parseFloat = function(num) {
+      return parseFloat(num);
+    }
   }
 
   ngOnInit() {
+    console.log("Current bill ID ", this.billId);
     Observable.forkJoin(
       this.http.get('/api/lobby/transaction/' + this.transaction).map((res: Response) => res.json())
     ).subscribe(
       data => {
         console.log(data[0])
         this.transactionBills.data = data[0];
-        this.currLobby.data = data[0].filter(function(lobby) {
-          console.log(lobby.bill_id)
-          if (lobby.bill_id === this.billId) {
-            return lobby;
-          }
-        });
-        console.log(this.currLobby);
+        this.transactionBills.issueCount = data[0].length;
+        this.first = data[0][0];
       },
       err => console.error(err)
       );
