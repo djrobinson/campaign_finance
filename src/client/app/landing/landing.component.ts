e61e90e7ad9aad28aefb7d70fdc730845fc94ae6
Danny Robinson <daniel@djrobinson.me>
2016-06-06 20:09:22 -0600

    Struggling with layots for landing page got something basic together

--- src/client/app/landing/landing.component.ts
+++ src/client/app/landing/landing.component.ts
@@ -3,23 +3,70 @@ import {Component} from 'angular2/core';
   selector: 'home',
   styles: [
   `
-    .landing-icon {
-      height: 600px;
+    .row {
+      background-color: #17324f;
+    }
+
+    .landing-container {
+      color: #17324f;
+      height: 100vh;
+      width: 100%;
+    }
+
+    #oval {
       display: flex;
-      flex-flow: column;
-      flex: 2;
       align-items: center;
       justify-content: center;
+      width: 50rem;
+      height: 30rem;
+      margin: 10% auto;
+      background: #CBCED2;
+      -moz-border-radius: 25rem / 15rem;
+      -webkit-border-radius: 25rem / 15rem;
+      border-radius: 25rem / 15rem;
     }
+
+    .campaign {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: #16191F;
+      height: 200px;
+      color: #CBCED2;
+    }
+
+    .text-center {
+      text-align: center;
+    }
+
+
   `
   ],
   template: `
-    <div class="landing-icon">
-      <h1>www.CitizensHub.org</h1>
-      <br>
-      <img src="http://www.opensecrets.org/wp-content/uploads/news/assets_c/2010/06/US-WhiteHouse-Logo-thumb-220x149-1250.png" />
-      <br>
-      <h4>Data Driven Politics</h4>
+
+    <div class="landing-container">
+      <div class="row">
+          <div class="twelve columns text-center">
+            <div id="oval">
+              <div class="title">
+                <h1>Citizens Hub</h1>
+                <h4>Data Driven Politics</h4>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="six columns text-center">
+          <div class="campaign six columns offset-by-three">
+            <h4>Campaign Finances</h4>
+          </div>
+        </div>
+        <div class="six columns text-center">
+          <div class="campaign six columns offset-by-three">
+            <h4>Lobbyists & Legislation</h4>
+          </div>
+        </div>
+      </div>
     </div>
   `
 })
