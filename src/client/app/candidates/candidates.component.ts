f94ba8aa8c7b73d4813625b6376af0372bbdf7c1
Danny Robinson <daniel@djrobinson.me>
2016-06-24 21:03:32 -0600

    Got candidates redirect and type working

--- src/client/app/candidates/candidates.component.ts
+++ src/client/app/candidates/candidates.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from 'angular2/core';
 import { RouteParams } from 'angular2/router';
-
+import {Http, Response} from 'angular2/http';
 
 @Component({
   selector: 'candidates-view',
@@ -13,14 +13,20 @@ export class CandidatesComponent implements OnInit {
   private sub: any;
 
   constructor(
-    // private route: ActivatedRoute,
-    // private router: Router,
-    private params: RouteParams
+    private _params: RouteParams,
+    private http: Http
     ){
-    this.type = params.get('type');
+    this.type = _params.get('type');
     console.log(this.type);
   }
 
   ngOnInit() {
+    this.http.get('/api/candidates/'+this.type+'/type')
+      .map(res => res.json())
+      .subscribe(
+        data => this.candidates = data,
+        err => this.logError(err),
+        () => console.log('Random Quote Complete')
+      );
   }
 }
