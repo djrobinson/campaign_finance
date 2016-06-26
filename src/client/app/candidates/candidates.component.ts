8d3280034f04c7b441b8aaf9f19ae3df28cc504c
Danny Robinson <daniel@djrobinson.me>
2016-06-26 08:20:05 -0600

    Dynamic styling to congressman page

--- src/client/app/candidates/candidates.component.ts
+++ src/client/app/candidates/candidates.component.ts
@@ -57,6 +57,13 @@ export class CandidatesComponent implements OnInit {
               this.headerType = 'House';
             }
             var finalData = data.map((item) => {
+              if (item.PARTY_CODE === 'DEM') {
+                item.tile_color = demColors[Math.floor(Math.random() * 3)];
+              } else if (item.PARTY_CODE === 'REP') {
+                item.tile_color = repColors[Math.floor(Math.random() * 3)];
+              } else {
+                item.tile_color = '#4C4664';
+              }
                this.http.get('/api/legislators/' + item.CANDIDATE_ID)
                  .map(res => res.json())
                  .subscribe(
@@ -64,7 +71,7 @@ export class CandidatesComponent implements OnInit {
                    console.log(secondData);
                    if (secondData[0].id){
                      item.profile_img = "https://raw.githubusercontent.com/unitedstates/images/gh-pages/congress/225x275/" + secondData[0].id.bioguide + ".jpg";
-                     return item;
+
                    }
                  },
                  err => console.log(err),
