e999d94fd84edca9fbc4dfe0be02bcb1795c0400
Danny Robinson <daniel@djrobinson.me>
2016-07-10 19:04:32 -0600

    Completed opex list popup tab

--- src/client/app/graphs/candidate-popup/opex/opex-list.component.ts
+++ src/client/app/graphs/candidate-popup/opex/opex-list.component.ts
@@ -2,11 +2,11 @@ import {Component, Input, Output, OnInit} from 'angular2/core';
 import {Http, Response} from 'angular2/http';
 
 @Component({
-  selector: 'individuals-list',
+  selector: 'opex-list',
   template: `
     <h1>Howdy</h1>
-    <ul *ngFor="#individual of individuals">
-      <li>{{individual}}</li>
+    <ul *ngFor="#opex of opexes">
+      <li>{{opex}}</li>
     </ul>
   `,
   styles: [`
@@ -15,17 +15,16 @@ import {Http, Response} from 'angular2/http';
   `],
   directives: []
 })
-export class IndividualsListComponent implements OnInit {
+export class OpexListComponent implements OnInit {
   @Input() cmte: string;
-  private individuals: any;
-
+  private opexes: any;
 
   constructor(private http:Http) {}
 
   ngOnInit(){
-    this.http.get('/api/individuals/' + this.cmte + '/recipient').map(response => response.json()).subscribe(data => {
+    this.http.get('/api/opex/committee/' + this.cmte ).map(response => response.json()).subscribe(data => {
       console.log(data);
-      this.individuals = data;
+      this.opexes = data;
     }, error => console.log('Could not load individuals.'));
   }
 }
--- src/client/app/graphs/candidate-popup/opex/opex.component.ts
+++ src/client/app/graphs/candidate-popup/opex/opex.component.ts
@@ -1,16 +1,22 @@
 import {Component, Input, Output, OnInit} from 'angular2/core';
 import {TreemapComponent} from '../../charts/treemap.component';
+import {OpexListComponent} from './opex-list.component';
 
 @Component({
   selector: 'opex-section',
   templateUrl: 'app/graphs/candidate-popup/opex/opex-partial.html',
   styleUrls: ['app/graphs/candidate-popup/opex/opex-partial.css'],
-  directives: [TreemapComponent]
+  directives: [TreemapComponent, OpexListComponent]
 })
 export class OpexSectionComponent implements OnInit {
   @Input() cmte: string;
   private route: string;
+  private showList: boolean=false;
   ngOnInit(){
     this.route = '/api/opex/aggregate/' + this.cmte;
+  }
+
+  public callList(){
+    this.showList = true;
   }
 }
